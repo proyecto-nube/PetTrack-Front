ce6a8551a62907c134e836878846012d724252dd
Parnol <arnolferneyperez@hotmail.com>
2025-11-22 17:38:57 -0500

    Delete clients/web-client directory

--- clients/web-client/src/api/petsService.js
+++
@@ -1,6 +0,0 @@
-// src/services/petsService.js
-import axios from "axios";
-const API_URL = "http://localhost:8001";
-
-export const getPets = async () => (await axios.get(`${API_URL}/pets`)).data;
-export const createPet = async (data) => (await axios.post(`${API_URL}/pets`, data)).data;
